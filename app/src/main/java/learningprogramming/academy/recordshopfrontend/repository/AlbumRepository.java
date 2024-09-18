6afa4e6ab8171d1e532fccbe486f6766228c63ee
Joeltt <joelttm15@gmail.com>
2024-09-18 12:16:14 +0100

    feat: Add AlbumRepository

--- app/src/main/java/learningprogramming/academy/recordshopfrontend/repository/AlbumRepository.java
+++ app/src/main/java/learningprogramming/academy/recordshopfrontend/repository/AlbumRepository.java
@@ -1,4 +1,43 @@
 package learningprogramming.academy.recordshopfrontend.repository;
 
+import android.app.Application;
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+
+import learningprogramming.academy.recordshopfrontend.model.Album;
+import learningprogramming.academy.recordshopfrontend.service.AlbumApiService;
+import learningprogramming.academy.recordshopfrontend.service.RetrofitInstance;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AlbumRepository {
+    private MutableLiveData<List<Album>> mutableLiveData = new MutableLiveData<>();
+    private Application application;
+
+    public AlbumRepository(Application application) {
+        this.application = application;
+    }
+
+    public MutableLiveData<List<Album>> getMutableLiveData() {
+        AlbumApiService albumApiService = RetrofitInstance.getService();
+        Call<List<Album>> call = albumApiService.getAllAlbums();
+
+        call.enqueue(new Callback<List<Album>>() {
+            @Override
+            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                List<Album> albumList = response.body();
+                mutableLiveData.setValue(albumList);
+            }
+
+            @Override
+            public void onFailure(Call<List<Album>> call, Throwable throwable) {
+                Log.e("HTTP failure", throwable.getMessage());
+            }
+        });
+        return mutableLiveData;
+    }
 }
